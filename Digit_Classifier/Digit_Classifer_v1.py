526629e5f4c3ea5262f8352ccc79bcc0da4a3216
Shresth Rana <shresthrana1@gmail.com>
2019-03-13 00:20:32 +0530

    Updated and Reviewed

---
+++ Digit_Classifier/Digit_Classifer_v1.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3)
+
+val_loss, val_accuracy = model.evaluate(x_test, y_test)
+print(val_loss,val_accuracy)
+
+predictions = model.predict([x_test])
+choice = 'Y'
+while choice=='Y' or choice =='y':
+	i = int(input("Enter the index for the databse(MNIST) for which you want to see the prediction :"))
+	if i >=0:
+		print(np.argmax(predictions[i]))
+		plt.imshow(x_test[i])
+		plt.show()
+	choice = input("Do you want to see another index?(Y/n)")
